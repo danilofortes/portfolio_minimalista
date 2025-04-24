2140a0b1fe0b9110d85f781c99a568f7bee326dd
Danilo Fortes <daniloaugusto.fortes@gmail.com>
2025-04-24 12:18:20 -0300

    news - dark and light mode, mobile responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ window.addEventListener('scroll', function () {
       head.style.height = '120px';
     }
   });
+
+let trilho = document.getElementById('trilho')
+let body =document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+  trilho.classList.toggle('dark')
+  body.classList.toggle('dark')
+})
+
